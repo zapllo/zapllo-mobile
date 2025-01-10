a57b534b45818747c971fbd2b7f223d5b63a989a
pooja <poojakri6144@gmail.com>
2025-01-10 13:47:33 +0530

    logged out functionality

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import ProfileButton from "../profile/ProfileButton";
+import { useSelector } from "react-redux";
+import { RootState } from "~/redux/store";
 
 interface NavbarProps {
   
@@ -8,6 +10,8 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({  title }) => {
+  
+
   return (
     <View className="w-full h-20 flex flex-row justify-between p-5 items-center">
       <Image className="w-11 h-7 " source={require("~/assets/home/logo.png")}/>

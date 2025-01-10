c458ab46f5bdf449b71c804cafa4d7b04379eccd
pooja <poojakri6144@gmail.com>
2025-01-10 17:03:20 +0530

    text size changes

--- components/TaskComponents/TaskCountSection.tsx
+++ components/TaskComponents/TaskCountSection.tsx
@@ -10,10 +10,10 @@ interface TaskStatusCardProps {
 const TaskStatusCard: React.FC<TaskStatusCardProps> = ({ imageSource, status, count }) => {
   return (
     <View className="p-5 items-start gap-3 flex flex-row border border-[#37384B] rounded-3xl w-1/2">
-      <Image className="h-8 w-8 mt-0.5" source={imageSource} />
+      <Image className="h-7 w-7" source={imageSource} />
       <View className="flex flex-col items-start">
-        <Text className="text-white text-sm" style={{fontFamily:"Lato-Regular"}}>{status}</Text>
-        <Text className="text-white text-4xl font-extrabold" style={{fontFamily:"Lato-Bold"}}>{count}</Text>
+        <Text className="text-white text-xl" style={{fontFamily:"LatoRegular"}}>{status}</Text>
+        <Text className="text-white text-3xl" style={{fontFamily:"LatoBold"}}>{count}</Text>
       </View>
     </View>
   );

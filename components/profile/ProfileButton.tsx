5828b02bc0ae3637284c7583c2c975f69436c4c5
pooja <poojakri6144@gmail.com>
2025-01-10 13:47:33 +0530

    logged out functionality

--- components/profile/ProfileButton.tsx
+++ components/profile/ProfileButton.tsx
@@ -1,14 +1,17 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import { router } from "expo-router";
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router } from 'expo-router';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/redux/store';
 
 export default function ProfileButton() {
+  const { isLoggedIn, token, userData } = useSelector((state: RootState) => state.auth);
+
   return (
-    <TouchableOpacity 
-    className="h-14 w-14 rounded-full"
-    onPress={()=>router.push("(routes)/profile" as any)}
-    >
+    <TouchableOpacity
+      className="h-14 w-14 rounded-full"
+      onPress={() => router.push('(routes)/profile' as any)}>
       <LinearGradient
         colors={['#815BF5', '#FC8929']}
         start={{ x: 0, y: 0 }}
@@ -19,9 +22,12 @@ export default function ProfileButton() {
           borderRadius: 50, // Ensure the gradient has the same border radius
           justifyContent: 'center',
           alignItems: 'center',
-        }}
-      >
-        <View className="bg-white h-12 w-12 rounded-full border-4 border-primary"></View>
+        }}>
+        {userData?.data?.profilePic ? (
+          <Image className='h-12 w-12 rounded-full border-4 border-primary' source={{ uri: userData?.data?.profilePic }} />
+        ) : (
+          <View className="h-12 w-12 rounded-full border-4 border-primary bg-white" />
+        )}
       </LinearGradient>
     </TouchableOpacity>
   );

497a1885ee5badc1c238b51ba684921ae8270887
subham <shubham@zapllo.com>
2025-01-21 23:19:50 +0530

    task categories add new task clicked

---
+++ app/(routes)/profile/billing/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import BillingScreen from "~/screens/profile/Billing/BillingScreen";
+
+export default function Billing() {
+  return (
+    <BillingScreen/>
+  );
+}

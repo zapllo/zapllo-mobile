8c49108514bb2c2896793a25ae17bf200084da6f
subham <shubham@zapllo.com>
2025-01-23 15:50:19 +0530

    change password

---
+++ app/(routes)/profile/ChangePassword/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import ChangePassWordScreen from "~/screens/profile/ChangePassword/ChangePassWordScreen";
+
+export default function ChangePassword() {
+  return (
+    <ChangePassWordScreen/>
+  );
+}

2d6d3ef893f82b462f1e5d70123210c86ba789be
subhamNative <shubham@zapllo.com>
2025-06-22 21:15:19 +0530

    sdk 53 install

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,11 @@ const { withNativeWind } = require('nativewind/metro');
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
+// Add resolver configuration to help with module resolution
+config.resolver = {
+  ...config.resolver,
+  unstable_enableSymlinks: false,
+  unstable_enablePackageExports: false,
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
